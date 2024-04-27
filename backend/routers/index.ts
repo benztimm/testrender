c49404a3056b07107946704cafa5e35961646f52
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-04-26 17:00:36 -0700

    add route for logout, pass in session for every render

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -5,15 +5,16 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',{session: req.session});
+  console.log(req.session);
 });
 
 router.get('/lobby', (req, res) => {
-  res.render('lobby');
+  res.render('lobby',{session: req.session});
 });
 
 router.get('/register', (req, res) => {
-  res.render('register');
+  res.render('register',{session: req.session});
 });
 
 
@@ -27,14 +28,23 @@ router.post('/register', async (req, res) => {
   
   // - Store all of them to DB
   await insertUser(username, hash, email);
-  res.render('login');
+  res.render('login',{session: req.session});
 });
 
 router.get('/login', (req:Request, res:Response) => {
-  res.render('login');
+  res.render('login',{session: req.session});
 });
 router.post('/login', (req:Request, res:Response) => { 
   loginRequest(req, res);
+});
+
+router.post('/logout', (req:Request, res:Response) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.redirect('/');
+    }
+    res.redirect('/login');
+  });
 });
 
 
