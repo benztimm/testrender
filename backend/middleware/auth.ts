bbf3c8f9d84cc19115ea960fbff7ed455e8a8296
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2024-05-03 18:55:19 -0700

    delete all comment, pass in session to render

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 
 declare module 'express-session' {
 	interface SessionData {
-		user?: { sessionID:string, username:string, // email:string
+		user?: { sessionID:string, username:string,
 		}
 	}
 }
@@ -36,7 +36,7 @@ const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
 	if (req.session.user) {
         next();
     } else {
-        res.redirect('/login');
+        res.render('login',{errorMessage: 'Please log in to access the page!', session: req.session});
     }
 }
 
@@ -48,15 +48,17 @@ const loginRequest = async (req: Request, res: Response) => {
 	const sessionID = req.session.id;
 
     const {username, password} = req.body
-	if (!username || !password) {
-		return res.render('login', {errorMessage: 'Please log in to access the game!'})
+	if (!username) {
+		return res.render('login', {errorMessage: 'Please enter your username', session: req.session})
+	}else if(!password){
+		return res.render('login', {errorMessage: 'Please enter your password!', session: req.session})
 	}
 	
 	if (await authenticate(username, password)) {
 		req.session.user = { sessionID , username }
 		return res.redirect('/')
 	} else {
-		return res.render('login', {errorMessage: 'Invalid username or password'})
+		return res.render('login', {errorMessage: 'Invalid username or password', session: req.session})
 	}
 }
 
